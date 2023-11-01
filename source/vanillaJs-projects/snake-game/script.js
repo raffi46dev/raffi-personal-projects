2ac4e8d73458d1d3e9a9d592c64b70038358065c
raffi46dev <raffi46work@gmail.com>
2023-11-01 23:54:18 +0530

    dimension increasing based on direction

--- source/vanillaJs-projects/snake-game/script.js
+++ source/vanillaJs-projects/snake-game/script.js
@@ -1,8 +1,8 @@
 const snake = document.getElementById('snake');
 const food = document.getElementById('food');
-const movementDelay = 200;
+const movementDelay = 500;
 const boardTimes = 100;
-let snake_Width = 1;
+let snake_Dimension = 1;
 let direction = {top:0, left:0};
 let foodArea = {};
 let intervalId;
@@ -18,10 +18,14 @@ function isSnakeFed(){
     }
     return false;
 }
-function growSnake(){
-    snake_Width += snake_Width;
-    snake.style.height += `${snake_Width}%`;
-    displayfood()
+function growSnake(direction){
+    snake_Dimension += 1;
+    if(direction == "top") {
+        snake.style.height += `${snake_Dimension}%`;
+    } else {
+        snake.style.width += `${snake_Dimension}%`;
+    }
+    displayfood();
 }
 
 function moveSnake(dir, negDir) {
@@ -37,11 +41,21 @@ function move(dir = 'left', negDir = false) {
     } else {
         direction[dir] += 1;
     }
-    snake.style[dir] = `${direction[dir]}%`; 
+    snake.style[dir] = `${direction[dir]}%`;
+    if(dir == "left" ) {
+        snake.style.width = `${snake_Dimension}%`;
+        snake.style.height = `2%`;
+        // snake.style.backgroundColor = "blue"
 
+    } else {
+        snake.style.height = `${snake_Dimension}%`;        
+        snake.style.width = `2%`;        
+        // snake.style.backgroundColor = "red"
+
+    }
 
     if(isSnakeFed() == true) {
-        growSnake();
+        growSnake(dir, negDir);
     }
 }
 function displayfood() {
